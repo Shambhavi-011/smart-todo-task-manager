4df0bc98c664e37f2edfb3fea4c78cb726194436
Shambhavi <shambhavi521reshu@gmail.com>
2026-08-28 22:26:57 +0530

    fixed filenames for deployment

--- server/server.js
+++ server/server.js
@@ -27,8 +27,8 @@ app.get("/", (req, res) => {
   res.json({ message: "Smart Task Manager API is running" });
 });
 
-app.use("/api/auth", authRoutes);
-app.use("/api/tasks", taskRoutes);
+app.use("/api/auth", authroutes);
+app.use("/api/tasks", taskroutes);
 
 const PORT = process.env.PORT || 5000;
 
